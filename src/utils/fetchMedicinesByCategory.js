4b726702c64d5e0f3693cbc06ceaf477db416b8e
Anahi Mayancela <anahimayancela@gmail.com>
2024-06-27 18:57:37 -0400

    lint

--- src/utils/fetchMedicinesByCategory.js
+++ src/utils/fetchMedicinesByCategory.js
@@ -3,7 +3,7 @@ const getMedicineNames = (significantLinkData) => {
     const names = []
 
     for(const entry of significantLinkData) {
-      
+
         // need to replace api call from production to sandbox
         const replacedUrl = entry.url.replace(
             "https://api.service.nhs.uk",
@@ -17,20 +17,20 @@ const getMedicineNames = (significantLinkData) => {
 }
 
 const fetchMedicinesByCategory = async (category) => {
-    const medicineInfo = [];
+  const medicineInfo = [];
 
-        try {
-          const res = await fetch(`https://sandbox.api.service.nhs.uk/nhs-website-content/medicines?category=${category}`);
-          const data = await res.json();
-          const parsedMedicineInfo = data && data.significantLink && getMedicineNames(data.significantLink);
+  try {
+    const res = await fetch(`https://sandbox.api.service.nhs.uk/nhs-website-content/medicines?category=${category}`);
+    const data = await res.json();
+    const parsedMedicineInfo = data && data.significantLink && getMedicineNames(data.significantLink);
 
-          parsedMedicineInfo && medicineInfo.push(...parsedMedicineInfo);
+    parsedMedicineInfo && medicineInfo.push(...parsedMedicineInfo);
 
-        } catch (error) {
-          console.error('Could not fetch medicines', error);
-        }
-    
-      return medicineInfo;
+  } catch (error) {
+    console.error('Could not fetch medicines', error);
+  }
+
+  return medicineInfo;
 };
 
 export default fetchMedicinesByCategory;
--- src/utils/fetchMedicinesByUrl.js
+++ src/utils/fetchMedicinesByUrl.js
@@ -1,30 +1,30 @@
 const fetchMedicinesByUrl = async (passedUrl) => {
-        try {
-          const res = await fetch(`${passedUrl}?modules=true`);
+    try {
+        const res = await fetch(`${passedUrl}?modules=true`);
+        const data = await res.json();
 
-          const data = await res.json();
-
-          if(!data) {
+        if(!data) {
             return null;
-          }
+        }
 
-         const {about, hasPart, headline, url } = data
+        const {about, hasPart, headline, url } = data
 
-          if(hasPart.length === 0) {
+        if(hasPart.length === 0) {
             return { [about.name]: { headline, url } };
-          }
+        }
 
         const filteredHasPart = hasPart.map(part => {
             const {hasPart, hasHealthAspect,...filteredPart } = part;
             return filteredPart;
         });
 
-          return { [about.name]: { info: filteredHasPart, headline, url } };
+        return { [about.name]: { info: filteredHasPart, headline, url } };
 
-        } catch (error) {
-          console.error(`Could not fetch medicine at ${passedUrl}`, error);
-          return null;
-        }
+    } 
+    catch (error) {
+        console.error(`Could not fetch medicine at ${passedUrl}`, error);
+        return null;
+    }
     
 };
 
