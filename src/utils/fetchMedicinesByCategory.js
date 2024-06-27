68b989640c8b40c98b17fe982dfb2d61dd48bd4f
Anahi Mayancela <anahimayancela@gmail.com>
2024-06-27 18:48:04 -0400

    fetch medicines by category

---
+++ src/utils/fetchMedicinesByCategory.js
@@ -0,0 +1,36 @@
+
+const getMedicineNames = (significantLinkData) => {
+    const names = []
+
+    for(const entry of significantLinkData) {
+      
+        // need to replace api call from production to sandbox
+        const replacedUrl = entry.url.replace(
+            "https://api.service.nhs.uk",
+            "https://sandbox.api.service.nhs.uk"
+        );
+
+        names.push(replacedUrl)
+    }
+
+    return names;
+}
+
+const fetchMedicinesByCategory = async (category) => {
+    const medicineInfo = [];
+
+        try {
+          const res = await fetch(`https://sandbox.api.service.nhs.uk/nhs-website-content/medicines?category=${category}`);
+          const data = await res.json();
+          const parsedMedicineInfo = data && data.significantLink && getMedicineNames(data.significantLink);
+
+          parsedMedicineInfo && medicineInfo.push(...parsedMedicineInfo);
+
+        } catch (error) {
+          console.error('Could not fetch medicines', error);
+        }
+    
+      return medicineInfo;
+};
+
+export default fetchMedicinesByCategory;
